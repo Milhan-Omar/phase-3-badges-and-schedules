da2b0ac893df63dca11fe03a2671c17f3bf6624f
Milhan Osman <milhanomarosman@gmail.com>
2023-05-16 17:04:34 +0300

    completed assigment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+ return "Hello, my name is #{name}."
+end
+# puts badge_maker("Ruby")
+
+def batch_badge_creator(array) 
+  array.collect{|value|badge_maker(value)}
+  end
+# puts batch_badge_creator(["aish","siham"]) 
+
+def assign_rooms(array)
+  array.map.each.with_index(1) do |value,index|
+   "Hello, #{value}! You'll be assigned to room #{index}!"
+   end
+end   
+
+
+def printer(arrays)
+ badge = batch_badge_creator(arrays)
+  badge.each do |badge|
+    puts badge
+    end
+
+ output = assign_rooms(arrays)
+  output.each do |output|
+    puts output
+  end
+end
